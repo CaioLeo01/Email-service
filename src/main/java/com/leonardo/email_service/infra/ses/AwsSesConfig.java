c3d6ff1d92e28c49b17eda9ac31d1a9eb18b08e2
Caio Manoel Costa Leonardo <caio.m.c.leonardo@gmail.com>
2025-11-08 19:55:45 -0300

    Fix: Correção de Confug amazon

--- src/main/java/com/leonardo/email_service/infra/ses/AwsSesConfig.java
+++ src/main/java/com/leonardo/email_service/infra/ses/AwsSesConfig.java
@@ -1,15 +1,25 @@
 package com.leonardo.email_service.infra.ses;
 
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
-
 @Configuration
 public class AwsSesConfig {
+
+    private final String region;
+
+    public AwsSesConfig(@Value("${aws.ses.region}") String region) {
+        this.region = region;
+    }
+
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        return AmazonSimpleEmailServiceClientBuilder.standard().build();
+        return AmazonSimpleEmailServiceClientBuilder
+                .standard()
+                .withRegion(region)
+                .build();
     }
 }
